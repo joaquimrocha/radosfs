df710b671ea52958dcef91ff008406997f8054a2
Joaquim Rocha <joaquim.rocha@cern.ch>
2014-02-12 16:34:59 +0100

    Add generic header for including everything at once
    Everything being RadosFs, RadosFsDir and RadosFsFile

---
+++ src/libradosfs.hh
@@ -0,0 +1,27 @@
+/*
+ * Rados Filesystem - A filesystem library based in librados
+ *
+ * Copyright (C) 2014 CERN, Switzerland
+ *
+ * Author: Joaquim Rocha <joaquim.rocha@cern.ch>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License at http://www.gnu.org/licenses/lgpl-3.0.txt
+ * for more details.
+ */
+
+#ifndef __LIB_RADOS_FS_HH__
+#define __LIB_RADOS_FS_HH__
+
+#include <radosfs/RadosFs.hh>
+#include <radosfs/RadosFsDir.hh>
+#include <radosfs/RadosFsFile.hh>
+
+#endif // __LIB_RADOS_FS_HH__
