b798648831b50799738d25ff2c16cad97c1f101b
Joaquim Rocha <joaquim.rocha@cern.ch>
2015-11-05 13:48:09 +0100

    bindings/python: Add wrapper module
    This is in order to keep the Python shared lib's name but being able
    to do 'import radosfs'.

---
+++ bindings/python/radosfs.py
@@ -0,0 +1,18 @@
+# Rados Filesystem - A filesystem library based in librados
+#
+# Copyright (C) 2014-2015 CERN, Switzerland
+#
+# Author: Joaquim Rocha <joaquim.rocha@cern.ch>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License at http://www.gnu.org/licenses/lgpl-3.0.txt
+# for more details.
+
+from libradosfspy import *
