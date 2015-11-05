3ae965929d1508833716541095239c79518a19de
Joaquim Rocha <joaquim.rocha@cern.ch>
2015-11-05 17:26:19 +0100

    python/test: Create/delete pools instead of using existent ones

--- bindings/python/test/tests.py
+++ bindings/python/test/tests.py
@@ -3,35 +3,53 @@ import argparse
 import errno
 import radosfs
 import unittest
+import rados
 import sys
+import uuid
 
 confPath = ''
 confUser = ''
-dataPool = ''
-metadataPool = ''
+POOLS_PREFIX = 'libradosfs-python-test'
 
 class RadosFsTestBase(unittest.TestCase):
 
+    metadataPool = POOLS_PREFIX + '-metadata-' + str(uuid.uuid1())
+    dataPool = POOLS_PREFIX + '-data-' + str(uuid.uuid1())
 
     def __init__(self, *args, **kwargs):
         unittest.TestCase.__init__(self, *args, **kwargs)
         self.fs = radosfs.Filesystem()
         self.assertEqual(self.fs.init(confUser, confPath), 0)
+        self.cluster = rados.Rados(conffile=confPath)
+        self.cluster.connect()
+
+    def createPools(self):
+        self.cluster.create_pool(self.dataPool)
+        self.cluster.create_pool(self.metadataPool)
+        self.mtdPoolCtx = self.cluster.open_ioctx(self.metadataPool)
+        self.dataPoolCtx = self.cluster.open_ioctx(self.dataPool)
+
+    def removePools(self):
+        self.mtdPoolCtx.close()
+        self.dataPoolCtx.close()
+        self.cluster.delete_pool(self.dataPool)
+        self.cluster.delete_pool(self.metadataPool)
 
     def setUp(self):
-        pass
+        self.createPools()
 
     def tearDown(self):
-        pass
+        self.removePools()
 
     def addPools(self):
-        self.assertEqual(self.fs.addMetadataPool(metadataPool, '/'), 0)
-        self.assertEqual(self.fs.addDataPool(dataPool, '/'), 0)
+        self.assertEqual(self.fs.addMetadataPool(self.metadataPool, '/'), 0)
+        self.assertEqual(self.fs.addDataPool(self.dataPool, '/'), 0)
 
     def testPoolsAdditionAndRemoval(self):
         self.addPools()
-        self.assertEqual(self.fs.removeMetadataPool(metadataPool), 0)
-        self.assertEqual(self.fs.removeDataPool(dataPool), 0)
+        self.assertEqual(self.fs.removeMetadataPool(self.metadataPool), 0)
+        self.assertEqual(self.fs.removeDataPool(self.dataPool), 0)
+
 
 def setupArguments():
     parser = argparse.ArgumentParser()
@@ -40,9 +58,6 @@ def setupArguments():
     parser._optionals.title = 'arguments'
     parser.add_argument('-c', '--conf', help="path to the Ceph cluster's "
                         "configuration file", required=True)
-    parser.add_argument('-p', '--pools', help="the names of the pools to use "
-                        "for the test", nargs=2,
-                        metavar=("METADATA_POOL", "DATA_POOL"), required=True)
     parser.add_argument('-u', '--user', help="the user name to connect to the "
                         "cluster")
     return parser
@@ -57,10 +72,7 @@ if __name__ == '__main__':
 
     confPath = args.conf
     confUser = args.user if args.user else ''
-    metadataPool = args.pools[0]
-    dataPool = args.pools[1]
 
-    print('Running tests on "%s" with user "%s" with the metadata pool "%s" and '
-          'data pool "%s"\n' % (confPath, confUser, metadataPool, dataPool))
+    print('Running tests on "%s" with user "%s"' % (confPath, confUser))
 
     unittest.main()
